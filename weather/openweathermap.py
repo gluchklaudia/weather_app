b11c317bdfa198c5402033cf3b5d0d5ade12e9c2
gluchklaudia <gluch.klaudiaa@gmail.com>
2022-03-07 12:48:34 -0500

    Add function get_city_coordinates to OpenWeatherMap class

--- weather/openweathermap.py
+++ weather/openweathermap.py
@@ -1,7 +1,24 @@
 import os
+import urllib
+import requests
+
 
 class OpenWeatherMap:
-    api_url = "http://api.openweathermap.org"
+    _api_url = "http://api.openweathermap.org"
+    _geocoding_endpoint_template = "geo/1.0/direct?q={city_name}&limit=1&appid={api_key}"
 
-    def __init__(self, api_key: str=None):
+    def __init__(self, api_key: str = None):
         self.api_key = api_key if api_key else os.environ["api_key"]
+
+    def get_city_coordinates(self, city_name: str):
+        endpoint = self._geocoding_endpoint_template.format(
+            api_key=self.api_key, city_name=city_name
+        )
+        url = urllib.parse.urljoin(self._api_url, endpoint)
+        response = requests.get(url)
+        if response.ok:
+            first_data = response.json()[0]
+            parsed_data = {"lat": first_data["lat"], "lon": first_data["lon"]}
+            return parsed_data
+        else:
+            print(response.status_code)
